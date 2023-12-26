23409ea232b900e98035ae398f3b348dedf4c4c5
Akash Biswas <64808720+akshbswas98@users.noreply.github.com>
2023-12-26 15:50:37 +0530

    added files

---
+++ my-app/src/routes/films/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    let film = 'The Usual Suspects'
+</script>
+<!-- <h1>{film.toUpperCase()}</h1> -->
+
+{#if film.startsWith('T')}
+    <p>Starts with 'T'</p>
+{:else}
+    <p>Doesn't starts with 'T'</p>
+{/if}
